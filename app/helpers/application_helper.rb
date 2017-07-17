922d9c3ea91ed01ac6cc9b5d090b7298cd0750df
Riccardo <riccardo.volpe1995@tiscali.it>
2017-07-17 12:56:06 +0200

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "UMarket"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
+
+# The full_title method is automagically available in all our views.
