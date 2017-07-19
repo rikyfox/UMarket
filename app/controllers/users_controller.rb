b847f8adb8ea479d864981438911b5b05a9c3682
Riccardo <riccardo.volpe1995@tiscali.it>
2017-07-19 13:32:12 +0200

    Use SSL and the Puma webserver in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])      # we’ve used params to retrieve the user id
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)           # see below the user_params method
+    if @user.save
+        flash[:success] = "Welcome to UMarket!"
+        redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
