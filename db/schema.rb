19446b84074b2811541040fc8fe79a4f09734981
Riccardo <riccardo.volpe1995@tiscali.it>
2017-09-07 15:41:25 +0200

    Add-user-role(prova)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720134558) do
+ActiveRecord::Schema.define(version: 20170721101415) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_assignments_on_role_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -32,14 +41,20 @@ ActiveRecord::Schema.define(version: 20170720134558) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
+    t.integer  "role"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
