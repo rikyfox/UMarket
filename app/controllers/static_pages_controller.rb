b05363ceb346932fd453475791a66f56cee8c142
root <root@localhost.localdomain>
2017-09-23 16:34:22 -0400

    modifica root_path + bottoni su article

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,19 @@
 class StaticPagesController < ApplicationController
+
   def home
     if logged_in? && current_user.vendor?
       @micropost  = current_user.microposts.build                           # current_user exists only if the user is logged in, so the @micropost variable should only be defined in this case.
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
+      @feed_items = current_user.feed.paginate(page: params[:page])  
   end
 
   def search
-	if params[:q]
-		@help=params[:q]
-		@articoli = Article.where("name LIKE ?", "%#{@help}%").paginate(page: params[:page])
-		@negozi = Market.where("name LIKE ?", "%#{@help}%").paginate(page: params[:page])
-	else
-		redirect_to root_path
-	end
-
-  end
+	   if params[:q]
+		     @help=params[:q]
+		       @articoli = Article.where("name LIKE ?", "%#{@help}%").paginate(page: params[:page])
+		         @negozi = Market.where("name LIKE ?", "%#{@help}%").paginate(page: params[:page])
+	          else
+		            redirect_to root_path
+	             end
+             end
+           end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,13 @@ ActiveRecord::Schema.define(version: 20170918073415) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "name"
+    t.integer  "prezzo"
     t.text     "description"
     t.boolean  "status"
     t.integer  "market_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "picture"
-    t.integer  "prezzo"
     t.index ["market_id"], name: "index_articles_on_market_id"
   end
 
